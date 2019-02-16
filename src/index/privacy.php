347020c8471e9ea9e761d4b9345780be3c99a653
gianfranco.branco <gianfranco.branco@studio.unibo.it>
2019-02-16 18:41:55 +0100

    I miss the privacy.php to update

--- src/index/privacy.php
+++ src/index/privacy.php
@@ -57,18 +57,20 @@ if(!controllo_cookie()){
 <html lang="it-IT">
 
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="keywords" content="Cibo, food, Just Eat, Just Uni Eat, just uni eat, asporto, università, fame, veloce, eat"/>
     <!--Bootstrap CSS-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
         crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="icon" href="http://example.com/favicon.png">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link href='https://fonts.googleapis.com/css?family=Faster One' rel='stylesheet'>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Just Uni Eat | Ristorante</title>
+    <link href="Toasty.js-master/dist/toasty.min.css" rel="stylesheet">
+    <title>Just Uni Eat | Privacy</title>
 </head>
 
 <body>
@@ -78,14 +80,42 @@ if(!controllo_cookie()){
             <span class="navbar-toggler-icon"></span>
         </button>
         <a class="navbar-brand" href="index.php">Just Uni Eat</a>
-        <a href="checkout.html">
-            <i class="material-icons md-36 carts">shopping_cart</i>
+        <a href="checkout.php">
+            <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "just_database";
+
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            if(isset($_SESSION['id'])) {
+                $idUs = $_SESSION['id'];
+                $checkCart = "SELECT ID_ORDINE FROM ordine WHERE ID_USER='$idUs' AND ORDINE_INVIATO=0";
+                $execControl = mysqli_query($conn, $checkCart);
+                $n_rows = mysqli_num_rows($execControl);
+    
+                if($n_rows === 0) {
+                ?>
+                <i class="material-icons md-36 carts">remove_shopping_cart</i>
+                <?php
+                } else if($n_rows > 0) {
+                ?>
+                <i class="material-icons md-36 carts">shopping_cart</i>
+                <?php
+                }
+                
+            }
+            ?>
         </a>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <div class="navbar-nav float-left text-left pr-3">
                 <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                        <a class="nav-link" id="navUser" href="#"></a>
+                        <a class="nav-link" id="navUser" href="profile.php"></a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" id="navAcc" href="accedi.php">Accedi</a>
@@ -95,14 +125,24 @@ if(!controllo_cookie()){
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" id="navMes" href="message.php">
-                          <i class="fa fa-envelope-o">
-                            <span class="badge badge-danger">1</span>
-                          </i>
-                          Messaggi
+                            <i class="fa fa-envelope-o">
+                                <span id="countMess" class="badge badge-danger">
+                                    
+                                <?php 
+                                      if($auth) {
+                                        $q= "SELECT COUNT(*) FROM utente AS U, messaggio AS M WHERE 
+                                        U.ID_USER='".$_SESSION["id"]."' AND U.ID_USER = M.ID_USER AND M.Letto='0' AND M.Ricevuto_Dal_Utente='1'";
+                                        $query=mysqli_query($conn, $q);
+                                        $result = mysqli_fetch_array($query);
+                                        echo $result['COUNT(*)']; 
+                                    } else echo "0";?>
+                                </span>
+                            </i>
+                            Messaggi
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" id="navOrd" href="#">Miei Ordini</a>
+                        <a class="nav-link" id="navOrd" href="mieiOrdini.php">Miei Ordini</a>
                         <!--da rendere hidden se non si ha fatto ancora l'accesso-->
                     </li>
                     <li class="nav-item">
@@ -308,46 +348,38 @@ In ogni caso avrai sempre diritto di proporre reclamo all’autorità di control
     <div class="content">
     </div>
     <footer id="myFooter">
-        <div class="container">
+        <div class="container text-center">
             <div class="row">
-                <div class="col-sm-3">
+                <div class="col-sm-4">
                     <h5>Inizia</h5>
                     <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Accedi</a></li>
-                        <li><a href="#">Registrati</a></li>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="accedi.php">Accedi</a></li>
+                        <li><a href="registrati.php">Registrati</a></li>
                     </ul>
                 </div>
-                <div class="col-sm-3">
+                <div class="col-sm-4">
                     <h5>Chi siamo</h5>
                     <ul>
-                        <li><a href="#">La Nostra Storia</a></li>
-                        <li><a href="#">Contattaci</a></li>
-                        <li><a href="#">Dicono di noi</a></li>
+                        <li><a href="storia.html">La Nostra Storia</a></li>
+                        <li><a href="contattaci.html">Contattaci</a></li>
+                        <li><a href="dicono_di_noi.html">Dicono di noi</a></li>
                     </ul>
                 </div>
-                <div class="col-sm-3">
-                    <h5>Fornitori</h5>
+                <div class="col-sm-4">
+                    <h5>Info</h5>
                     <ul>
-                        <li><a href="#">Elenco completo</a></li>
-                        <li><a href="#">Diventa affiliato</a></li>
-                        <li><a href="#">Diventa fattorino</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-3">
-                    <h5>Termini</h5>
-                    <ul>
-                        <li><a href="#">Termini del servizio</a></li>
-                        <li><a href="#">Termini di utilizzo</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="privacy.php">Privacy & Cookie</a></li>
+                        <li><a href="registrati.php">Diventa affiliato</a></li>
+                        <li><a href="diventa_fattorino.php">Diventa fattorino</a></li>
                     </ul>
                 </div>
             </div>
         </div>
         <div class="social-networks">
-            <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-            <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-            <a href="#" class="google"><i class="fa fa-google-plus"></i></a>
+            <a target="_blank" href="https://twitter.com/JustUniEat1" class="twitter"><i class="fa fa-twitter"></i></a>
+            <a target="_blank" href="https://www.facebook.com/justuni.eat.5" class="facebook"><i class="fa fa-facebook"></i></a>
+            <a target="_blank" href="https://plus.google.com/u/0/114848465565497583176" class="google"><i class="fa fa-google-plus"></i></a>
         </div>
         <div class="footer-copyright">
             <p>© 2018 Copyright Just Uni Eat</p>
@@ -355,12 +387,13 @@ In ogni caso avrai sempre diritto di proporre reclamo all’autorità di control
     </footer>
     </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
         crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
         crossorigin="anonymous"></script>
+    <script src="Toasty.js-master/dist/toasty.min.js"></script>
 
         <?php
 
@@ -381,6 +414,34 @@ In ogni caso avrai sempre diritto di proporre reclamo all’autorità di control
             document.getElementById('navExit').style.display = "block";
         });
         
+        var ajax_call = function() {
+        
+        var id_user = <?php echo $_SESSION['id'];?>
+
+        $.ajax({
+
+        url : 'checkMessageNew.php',
+        method : 'post',
+        data : {id_user : id_user},
+
+            success : function(response) {
+
+                if(response == "1") {
+                    var toast = new Toasty();
+                    //toast.progressBar("true");
+                    toast.success("Hai un nuovo messaggio!");
+                    $('#countMess').text("1");
+                }    
+            
+            }
+
+        });
+
+    };
+
+    var interval = 30000; //30 secondi
+
+    setInterval(ajax_call, interval);
         </script>
     <?php
     } else {
