4eca248baaabdbfd6f2f545a9acfa80b28cdae61
gianfranco.branco <gianfranco.branco@studio.unibo.it>
2019-02-19 08:12:02 +0100

    add some css and style in listinoF

--- src/Fornitori/ListinoF.php
+++ src/Fornitori/ListinoF.php
@@ -51,27 +51,35 @@ $rows = $result->num_rows;
  <!DOCTYPE html>
  <html lang="it-IT">
  <head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-     <!--Bootstrap CSS-->
-     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-     <link rel="stylesheet" href="style.css">
-     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
-     <link rel="icon" href="http://example.com/favicon.png">
+ <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="keywords" content="Cibo, food, Just Eat, Just Uni Eat, just uni eat, asporto, università, fame, veloce, eat"/>
+    <!--Bootstrap CSS-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="icon" href="http://example.com/favicon.png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="style.css">
+    <link href='https://fonts.googleapis.com/css?family=Faster One' rel='stylesheet'>
+    <style>
+    
+    .navbar-brand {
+    font-family: 'Faster One';
+    font-size: 25px;
+}
+    </style>
      <title>Just Uni Eat Fornitori - Il tuo Listino</title>
  </head>
 
  <body>
-   <!---HEADER--->
    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-         <span class="navbar-toggler-icon"></span>
-       </button>
-       <a class="navbar-brand" href="#">Just Uni Eat</a>
-        <a href="#">
-          <img class="carts" src="../../doc/shopping-cart-empty-side-view.png" alt="shopping carts">
-         </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+       <a class="navbar-brand" href="HomeF.php">Just Uni Eat</a>
        <div class="collapse navbar-collapse" id="navbarSupportedContent">
          <div class="navbar-nav float-left text-left pr-3">
            <ul class="navbar-nav mr-auto">
@@ -85,10 +93,10 @@ $rows = $result->num_rows;
                 <a class="nav-link" href="#">Notifiche</a> <!--da rendere hidden se non si ha fatto ancora l'accesso-->
              </li>
              <li class="nav-item">
-                 <a class="nav-link" href="#">Miei Ordini</a> <!--da rendere hidden se non si ha fatto ancora l'accesso-->
+                 <a class="nav-link" href="OrdiniF.php">Miei Ordini</a> <!--da rendere hidden se non si ha fatto ancora l'accesso-->
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Esci</a> <!--da rendere hidden se non si ha fatto ancora l'accesso-->
+                <a class="nav-link" href="../index/logout.php">Esci</a> <!--da rendere hidden se non si ha fatto ancora l'accesso-->
              </li>
            </ul>
         </div>
@@ -99,11 +107,12 @@ $rows = $result->num_rows;
 
 <div id="accordion" class="container col-sm-12 col-md-8">
 
-  <h1 style="text-align:center"><?php echo $_SESSION["Ristorante"] . "<br>";?></h1>
-  <h2 style="text-align:center">IL TUO LISTINO</h2>
+  <h1 class="text-center onBoard"><?php echo $_SESSION["Ristorante"] . "<br>";?></h1>
+  <hr class="onBoard-hr">
+  <h2 class="text-center onBoard onBoard-space">IL TUO LISTINO</h2>
   <div class="card">
     <div class="card-header" id="headingTitle">
-      <h5 class="mb-0" style="text-align:center"> I TUOI PIATTI</h5>
+      <h5 class="mb-0 text-center"> I TUOI PIATTI</h5>
 
     </div>
   </div>
@@ -341,17 +350,16 @@ $rows = $result->num_rows;
 
   <!-- CATEGORIE -->
   <div class="container-fluid col-sm-12 col-md-8 col-lg-8">
-  <h5 class="mb-0" style="text-align:center; margin-top:1em;"> LE TUE CATEGORIE</h5>
+  <h5 class="mb-0 text-center onBoard onBoard-space-md" >LE TUE CATEGORIE</h5>
 
-  <div class="Categorie container-fluid">
-    <label class="row" for="ContainerCategorie">Categorie:</label>
+  <div class="Categorie container-fluid onBoard-space-md">
     <div class="row container-fluid">
-    <div name="ContainerCategorie" class="containerCategorie col-sm-6 col-lg-6" style="border:1px solid black;">
+    <div name="ContainerCategorie" class="containerCategorie col-sm-6 col-lg-6 onBoard" style="border:2px solid white;">
       <!-- ADD CHECKBOXES -->
       <?php if($categoria == NULL)
               echo "Non hai scelto categorie.";
             else { do{
-                      echo $categoria." <br>";
+                      echo $categoria."<br>";
                     } while($QueryCatF->fetch());
             }
 
@@ -433,10 +441,29 @@ $QueryAddCat->close();
 
 </script>
 
-
+<div class="content">
+    </div>
+    <footer id="myFooter">
+       
+        <div class="social-networks">
+            <a target="_blank" href="https://twitter.com/JustUniEat1" class="twitter"><i class="fa fa-twitter"></i></a>
+            <a target="_blank" href="https://www.facebook.com/justuni.eat.5" class="facebook"><i class="fa fa-facebook"></i></a>
+            <a target="_blank" href="https://plus.google.com/u/0/114848465565497583176" class="google"><i class="fa fa-google-plus"></i></a>
+        </div>
+        <div class="footer-copyright">
+            <p>© 2018 Copyright Just Uni Eat</p>
+        </div>
+    </footer>
+    </div>
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+    <script src="Toasty.js-master/dist/toasty.min.js"></script>
 
  </body>
- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+ 
 </html>
