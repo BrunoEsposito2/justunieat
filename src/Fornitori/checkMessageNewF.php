74a4b8ab14833d299318de76a72dda824e83dbc9
Francesco Agnoletti <francesco.agnoletti3@studio.unibo.it>
2019-02-19 17:30:23 +0100

    Fornitori message now working good

---
+++ src/Fornitori/checkMessageNewF.php
@@ -0,0 +1,25 @@
+<?php
+ $servername = "localhost";
+ $username = "root";
+ $password = "";
+ $dbname = "just_database";
+
+ $connection = new mysqli($servername, $username, $password, $dbname);
+ if ($connection->connect_error) {
+     die("Connection failed: " . $connection->connect_error);
+ }
+
+
+	$id_user = $_POST['id_user'];
+
+
+	$result  = mysqli_query($connection , "SELECT * FROM messaggio WHERE ID_RISTORANTE = $id_user AND Letto = 0 AND Ricevuto_Dal_Utente = 0");
+
+	if(mysqli_num_rows($result) > 0) {
+		echo "1";
+	} else {
+		echo "0";
+	}
+
+
+?>
---
+++ src/Fornitori/updateMessageCountF.php
@@ -0,0 +1,16 @@
+<?php
+ $servername = "localhost";
+ $username = "root";
+ $password = "";
+ $dbname = "just_database";
+
+ $connection = new mysqli($servername, $username, $password, $dbname);
+ if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+ }
+
+    $id = $_POST["id"];
+
+	$result  = mysqli_query($connection , "UPDATE messaggio SET Letto=1 WHERE ID_RISTORANTE='$id'");
+    $conn->close();
+?>
