fef6b409952a96b64ba0b418e93e89ec48f7ee59
Francesco Agnoletti <francesco.agnoletti3@studio.unibo.it>
2019-02-20 03:15:13 +0100

    Fixed modal

--- src/Fornitori/ListinoF.php
+++ src/Fornitori/ListinoF.php
@@ -201,7 +201,7 @@ if(isset($_GET["cats"])){
             <div class="modal-dialog modal-dialog-centered" role="document">
               <div class="modal-content">
                 <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                  <h5 class="modal-title" id="exampleModalLongTitle">Modifica pietanza '.$ris["Nome"].'</h5>
                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                   </button>
@@ -271,7 +271,7 @@ if(isset($_GET["cats"])){
                   </div>
                   </form>
                   <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Chiudi</button>
                   </div>
               </div>
             </div>
