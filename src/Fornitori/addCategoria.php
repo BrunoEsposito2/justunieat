6db559fd5679ed0df8f6ea8ccfe551a10e056137
gianfranco.branco <gianfranco.branco@studio.unibo.it>
2019-02-20 10:03:23 +0100

    fix little bug connection DB

--- src/Fornitori/addCategoria.php
+++ src/Fornitori/addCategoria.php
@@ -1,10 +1,11 @@
 <?php
 
-define("HOST", "localhost"); // E' il server a cui ti vuoi connettere
-define("USER", "admin_user"); // E' l'utente con cui ti collegherai al DB.
-define("PASSWORD", "Justunieat2019"); // Password di accesso al DB.
-define("DATABASE", "just_database"); // Nome del database.
-$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+$servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "just_database";
+
+        $mysqli = new mysqli($servername, $username, $password, $dbname);
 
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
